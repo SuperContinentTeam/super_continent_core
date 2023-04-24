73cea91788354bee88980bc727e829bef0f20cdc
h2magic <h2magic@163.com>
2023-04-24 15:16:18 +0800

    finish mult-websocket-server

--- src/connections.rs
+++ src/connections.rs
@@ -4,7 +4,7 @@ use once_cell::sync::OnceCell;
 use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::TcpStream;
 use tokio_tungstenite::{
     accept_async,
     tungstenite::{Error, Message},
@@ -20,6 +20,19 @@ pub fn initial_peer_map() {
     PEER_MAP.set(result).unwrap();
 }
 
+pub fn parse_text_message(text: String, peer: &SocketAddr) {
+    let peer_map = PEER_MAP.get().unwrap().clone();
+    println!("Received a message from {}: {}", peer, text);
+    let message = Message::Text(format!("{}: {}", &peer, text));
+
+    let peer_map_guard = peer_map.lock().unwrap();
+    let clients = peer_map_guard.iter().map(|(addr, ws_sink)| (addr, ws_sink));
+    for (addr, client) in clients {
+        println!("Send Message to: {}", addr);
+        client.unbounded_send(message.clone()).unwrap();
+    }
+}
+
 pub async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Result<(), Error> {
     let peer_map = PEER_MAP.get().unwrap().clone();
 
@@ -32,16 +45,18 @@ pub async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Result<()
     let (outgoing, incoming) = ws_stream.split();
 
     let incoming_future = incoming.try_for_each(|msg| {
-        let text = msg.to_text().unwrap();
-        println!("Received a message from {}: {}", peer, text);
-        let message = Message::Text(format!("{}: {}", &peer, text));
-
-        let peer_map_guard = peer_map.lock().unwrap();
-        let clients = peer_map_guard.iter().map(|(addr, ws_sink)| (addr, ws_sink));
-        for (addr, client) in clients {
-            println!("Send Message to: {}", addr);
-            client.unbounded_send(message.clone()).unwrap();
+        match msg {
+            Message::Text(text) => parse_text_message(text, &peer),
+            Message::Binary(_) => todo!(),
+            Message::Ping(_) => todo!(),
+            Message::Pong(_) => todo!(),
+            Message::Close(_) => {
+                println!("{} disconnected", &peer);
+                PEER_MAP.get().unwrap().lock().unwrap().remove(&peer);
+            },
+            Message::Frame(_) => todo!(),
         }
+
         future::ok(())
     });
 
@@ -49,34 +64,6 @@ pub async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Result<()
 
     pin_mut!(incoming_future, outgoing_future);
     future::select(incoming_future, outgoing_future).await;
-
-    println!("{} disconnected", &peer);
-    PEER_MAP.get().unwrap().lock().unwrap().remove(&peer);
-
-    // while let Some(msg) = rx.next().await {
-    //     match msg {
-    //         Ok(Message::Text(text)) => {
-    //             println!("Get Message: {}", text);
-    //             let msg = Message::Text(format!("{}: {}", peer.to_string(), text));
-    //             // tx.send(msg).await.unwrap();
-    //             // {
-    //             //     let sender_map = SENDER_MAP.clone();
-    //             //     let mut sender_map_clone = sender_map.lock().unwrap();
-    //             //     for (_, ws) in sender_map_clone.iter_mut() {
-    //             //         ws.send(msg.clone()).unwrap();
-    //             //     }
-    //             // }
-
-    //             // let message = game::command_executor(get_game_state(), &text).await;
-    //         }
-    //         Ok(Message::Close(_)) => {
-    //             println!("Client disconnect");
-    //             break;
-    //         }
-    //         Ok(_) => (),
-    //         Err(e) => return Err(e),
-    //     }
-    // }
 
     Ok(())
 }
