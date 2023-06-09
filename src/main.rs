cc055d016440ee3166d152f8f3ab1552ce7c1077
h2magic <h2magic@163.com>
2023-06-09 14:53:42 +0800

    replace of websocket using normal socket

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,53 @@
-use std::net::SocketAddr;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 
-use dotenv::dotenv;
-use tokio::net::TcpListener;
+fn main() {
+    let listener = TcpListener::bind("0.0.0.0:55555").unwrap();
+    println!("Listening 0.0.0.0:55555...");
+    // let mut thread_vec: Vec<thread::JoinHandle<()>> = Vec::new();
 
-mod game;
-// mod redis_client;
-
-mod state {
-    pub mod state;
-    pub mod tick;
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                // 创建新线程处理客户端连接
+                thread::spawn(move || handle_socket(stream));
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
 }
 
-// mod room {
-//     pub mod room;
-// }
+fn handle_socket(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let mut received_data = String::new();
+    let peer_addr = stream.peer_addr().unwrap();
+    println!("已连接: {}", peer_addr);
 
-mod connections;
-mod queue;
-
-#[tokio::main]
-async fn main() {
-    dotenv().ok();
-
-    connections::initial_peer_map();
-    let addr: SocketAddr = "0.0.0.0:55555".parse().unwrap();
-    let listener = TcpListener::bind(&addr).await.expect("Can't listen");
-    println!("Listening on: {}", addr);
-
-    // WebSocket Server
     loop {
-        match listener.accept().await {
-            Ok((stream, peer)) => {
-                println!("Peer address: {}", peer);
-                tokio::spawn(connections::accept_connection(peer, stream));
+        match stream.read(&mut buffer) {
+            Ok(bytes_read) if bytes_read > 0 => {
+                // 将接收到的数据转换为字符串
+                let data = String::from_utf8_lossy(&buffer[..bytes_read]);
+
+                // 将接收到的数据拼接到已接收数据的末尾
+                received_data.push_str(&data);
+                if received_data.ends_with("\n") {
+                    println!("接收信息为: {}", received_data);
+                    received_data.clear();
+                }
+
+                continue;
             }
-            Err(_) => {
-                eprintln!("connected streams should have a peer address");
+            Ok(_) => {
+                println!("Disconnect socket");
+                break;
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
             }
         }
     }
