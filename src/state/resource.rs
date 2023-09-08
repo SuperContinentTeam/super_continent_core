2daf5f5b301a8d5bbbabb1791dad415525398778
h2magic <h2magic@163.com>
2023-09-08 10:30:48 +0800

    set resource state

--- src/state/resource.rs
+++ src/state/resource.rs
@@ -1,22 +1,69 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
 pub enum ResourceType {
-    TICK,
     ENERGY,
     MINERAL,
     FOOD,
     CUSTOMER,
     ALLOY,
-    PHYSICS,
-    ENGINEER,
-    BEYOND,
+    TECHNOLOGY,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct Resource {
     pub typ: ResourceType,
-    pub material: Option<Vec<Self>>,
+    pub storage: i64,
+    pub daily: i64,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct StateResource {
-    pub resource: Resource,
-    pub storage: f64,
-    pub daily: f64,
+    pub energy: Resource,
+    pub mineral: Resource,
+    pub food: Resource,
+    pub customer: Resource,
+    pub alloy: Resource,
+    pub technology: Resource,
+}
+
+impl StateResource {
+    pub fn new() -> Self {
+        Self {
+            energy: Resource {
+                typ: ResourceType::ENERGY,
+                storage: 1000,
+                daily: 10,
+            },
+            mineral: Resource {
+                typ: ResourceType::MINERAL,
+                storage: 1000,
+                daily: 10,
+            },
+            food: Resource {
+                typ: ResourceType::FOOD,
+                storage: 1000,
+                daily: 10,
+            },
+            customer: Resource {
+                typ: ResourceType::CUSTOMER,
+                storage: 500,
+                daily: 5,
+            },
+            alloy: Resource {
+                typ: ResourceType::ALLOY,
+                storage: 500,
+                daily: 5,
+            },
+            technology: Resource {
+                typ: ResourceType::TECHNOLOGY,
+                storage: 300,
+                daily: 3,
+            },
+        }
+    }
+
+    pub fn display(&self) -> serde_json::Value {
+        serde_json::json!(self)
+    }
 }
--- src/state/state.rs
+++ src/state/state.rs
@@ -1,8 +1,12 @@
 use std::error::Error;
+use serde::{Deserialize, Serialize};
+use crate::state::resource::StateResource;
 
+#[derive(Deserialize, Serialize)]
 pub struct State {
     pub tick: u64,
     pub name: String,
+    pub state_resource: StateResource,
 }
 
 
@@ -11,12 +15,11 @@ impl State {
         State {
             tick: 0,
             name,
+            state_resource: StateResource::new(),
         }
     }
 
     pub fn next(&mut self) {
         self.tick += 1;
-
-        println!("State: {}, tick: {}", self.name, self.tick);
     }
 }
