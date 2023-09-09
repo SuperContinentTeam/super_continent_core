e7b8ab1f1ecf115e376a8f18657dc7dd4791f113
h2magic <soleperson@126.com>
2023-09-09 10:06:27 +0800

    new function: event bus

--- src/event_bus/mod.rs
+++ src/event_bus/mod.rs
@@ -8,37 +8,32 @@ type Listener = fn(serde_json::Value);
 type Listeners = Vec<Listener>;
 
 lazy_static! {
-    static ref EVENT_MAP: Arc<Mutex<HashMap<String, Vec<Listener>>>> = Arc::new(Mutex::new(HashMap::new()));
+    static ref EVENT_MAP: Arc<Mutex<HashMap<String, Listeners>>> = Arc::new(Mutex::new(HashMap::new()));
 }
 
 
-pub fn register(name: String, listener: Listener) {
+pub fn register(name: &str, listener: Listener) {
     let event_clone = EVENT_MAP.clone();
     let mut event_map = event_clone.lock().unwrap();
-    match event_map.get_mut(&name) {
-        None => {}
-        Some(_) => {}
+
+    match event_map.get_mut(name) {
+        None => {
+            let listeners = vec![listener];
+            event_map.insert(name.to_string(), listeners);
+        }
+        Some(listeners) => {
+            listeners.push(listener);
+        }
     }
 }
 
-// pub fn register<T>(&mut self, name: String, listener: Listener)
-// {
-//     match self.collections.get_mut(&name) {
-//         Some(listeners) => {
-//             listeners.push(listener);
-//         }
-//         None => {
-//             let listeners = vec![listener];
-//             self.collections.insert(name, listeners);
-//         }
-//     }
-// }
-//
-// pub fn emit(&self, name: String, value: serde_json::Value) {
-//     if let Some(listeners) = self.collections.get(&name) {
-//         for listener in listeners {
-//             listener(value.clone());
-//         }
-//     }
-// }
-// }
+pub fn emit(name: &str, value: &serde_json::Value) {
+    let event_clone = EVENT_MAP.clone();
+    let event_map = event_clone.lock().unwrap();
+
+    if let Some(listeners) = event_map.get(name) {
+        for listener in listeners {
+            listener(value.clone());
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,20 @@
-use std::str::FromStr;
-use std::sync::{Arc, Mutex};
-use std::thread;
-
-use chrono::Local;
-use tokio;
-
 mod state;
 mod event_bus;
 
 
 fn main() {
-    let name = serde_json::json!({"name": "A"});
-    state::manager::add_state(name);
-
+    let name1 = serde_json::json!({"name": "A"});
     let name2 = serde_json::json!({"name": "B"});
-    state::manager::add_state(name2);
+
+    event_bus::register("AddState", |value| {
+        state::manager::add_state(value);
+    });
+
+    event_bus::emit("AddState", &name1);
+    event_bus::emit("AddState", &name2);
 
     std::thread::sleep(std::time::Duration::from_secs(3600));
 
-    // let ax_manager = Arc::new(Mutex::new(StateManager::new()));
-    // let manager_clone = ax_manager.clone();
-    //
-    // let thread_state = thread::spawn(move || {
-    //     let rt_state = tokio::runtime::Builder::new_current_thread().enable_all().unwrap();
-    //
-    //     println!("启动状态机: {}", Local::now().format("%F %T"));
-    //     rt_state.block_on(async {
-    //         let tick = tokio::time::Duration::from_secs(1);
-    //
-    //         loop {
-    //             let mut manager = manager_clone.lock().unwrap();
-    //             manager.next().await;
-    //             tokio::time::sleep(tick).await;
-    //         }
-    //         // println!("结束状态机: {}", Local::now().format("%F %T"));
-    //     });
-    // });
-    //
-    // thread_state.join().unwrap();
     // let thread_net = thread::spawn(|| {
     //     let rt_net = tokio::runtime::Runtime::new().unwrap();
     // });
