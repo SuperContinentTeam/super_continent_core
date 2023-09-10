74b536f3d51774531f3ca4a10b0eb67615bee563
h2magic <soleperson@126.com>
2023-09-10 19:08:40 +0800

    finish send and receive

--- src/ws.rs
+++ src/ws.rs
@@ -1,16 +1,16 @@
-use std::str::FromStr;
+use fastwebsockets::{upgrade, FragmentCollector, Frame, OpCode, Payload, WebSocketError};
+use hyper::{server::conn::Http, service::service_fn, upgrade::Upgraded, Body, Request, Response};
+use lazy_static::lazy_static;
+use serde_json::{json, Value};
+use std::{collections::HashMap, str::FromStr, sync::Arc};
+use tokio::{net::TcpListener, sync::Mutex};
 
-use fastwebsockets::Frame;
-use fastwebsockets::Payload;
-use fastwebsockets::upgrade;
-use fastwebsockets::OpCode;
-use fastwebsockets::WebSocketError;
-use hyper::server::conn::Http;
-use hyper::service::service_fn;
-use hyper::Body;
-use hyper::Request;
-use hyper::Response;
-use tokio::net::TcpListener;
+type AXController = Arc<Mutex<FragmentCollector<Upgraded>>>;
+type PeerMap = Arc<Mutex<HashMap<String, HashMap<String, AXController>>>>;
+
+lazy_static! {
+    static ref PEER_MAP: PeerMap = PeerMap::default();
+}
 
 pub async fn start_server() {
     let bind_addr = "0.0.0.0:7000".to_string();
@@ -42,26 +42,51 @@ async fn server_upgrade(mut req: Request<Body>) -> Result<Response<Body>, WebSoc
     Ok(response)
 }
 
+fn parse_to_value(payload: &Payload) -> Value {
+    let u8_content = payload.to_vec();
+    let content = std::str::from_utf8(&u8_content).unwrap();
+    Value::from_str(content).unwrap()
+}
+
 async fn handle_client(fut: upgrade::UpgradeFut) -> Result<(), WebSocketError> {
-    let mut ws = fastwebsockets::FragmentCollector::new(fut.await?);
+    let ws = fastwebsockets::FragmentCollector::new(fut.await?);
+    let ax_ws = Arc::new(Mutex::new(ws));
 
     loop {
-        let frame = ws.read_frame().await?;
+        let ws_clone = ax_ws.clone();
+        
+        let frame = {
+            let mut ws_gurand = ws_clone.lock().await;
+            ws_gurand.read_frame().await?
+        };
+
         match frame.opcode {
             OpCode::Close => break,
             OpCode::Text | OpCode::Binary => {
                 let value = parse_to_value(&frame.payload);
                 println!("{:#?}", value);
 
-                let resp = serde_json::json!({
+                if let Some(op) = value.get("op") {
+                    let op = op.as_str().unwrap();
+                    match op {
+                        "join" => {
+                            let _ = join_room(&value, ws_clone.clone()).await;
+                        }
+                        _ => {}
+                    }
+                }
+
+                let resp = json!({
                     "op": "hey",
                     "payloady": value
-                }).to_string();
-                let resp_clone = resp.clone();
-                let resp_vec_u8 = resp_clone.as_bytes();
-                let payload = Payload::from(resp_vec_u8);
-                let new_frame = Frame::new(true, frame.opcode, None, payload);
-                ws.write_frame(new_frame).await?;
+                });
+
+                send_message(&resp, &ws_clone).await;
+                // let resp_clone = resp.clone();
+                // let resp_vec_u8 = resp_clone.as_bytes();
+                // let payload = Payload::from(resp_vec_u8);
+                // let new_frame = Frame::new(true, frame.opcode, None, payload);
+                // ws_gurand.write_frame(new_frame).await?;
             }
             _ => {}
         }
@@ -70,9 +95,46 @@ async fn handle_client(fut: upgrade::UpgradeFut) -> Result<(), WebSocketError> {
     Ok(())
 }
 
+async fn send_message(message: &Value, receiver: &AXController) {
+    let receiver_clone = receiver.clone();
+    let str_message = message.to_string();
+    let u8_message = str_message.as_bytes();
+    let payload = Payload::from(u8_message);
+    let frame = Frame::new(true, OpCode::Text, None, payload);
+    let mut receiver_guard = receiver_clone.lock().await;
+    receiver_guard.write_frame(frame).await.unwrap();
+}
 
-fn parse_to_value(payload: &Payload) -> serde_json:: Value {
-    let u8_content  = payload.to_vec();
-    let content = std::str::from_utf8(&u8_content).unwrap();
-    serde_json::Value::from_str(content).unwrap()
+async fn broadcast(room: &str, message: &Value) {
+    let peer_map_clone = PEER_MAP.clone();
+    let peer_map = peer_map_clone.lock().await;
+
+    if let Some(ws_map) = peer_map.get(room) {
+        for ws in ws_map.values() {
+            send_message(message, ws).await;
+        }
+    }
+}
+
+async fn join_room(message: &Value, websocket: AXController) -> Value {
+    let name = message.get("name").unwrap().as_str().unwrap();
+    let room = message.get("room").unwrap().as_str().unwrap();
+
+    let peer_map_clone = PEER_MAP.clone();
+    let mut peer_map = peer_map_clone.lock().await;
+
+    match peer_map.get_mut(room) {
+        Some(ws_map) => {
+            if !ws_map.contains_key(name) {
+                ws_map.insert(name.to_string(), websocket.clone());
+            }
+        }
+        None => {
+            let mut ws_map: HashMap<String, AXController> = HashMap::new();
+            ws_map.insert(name.to_string(), websocket.clone());
+            peer_map.insert(room.to_string(), ws_map);
+        }
+    }
+
+    json!({"status": "success"})
 }
