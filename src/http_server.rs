7dc55ce9d1e98982b0a31ab2a0ffd93745ded402
h2magic <h2magic@126.com>
2023-09-14 15:07:54 +0800

    add api for  query rooms

--- src/http_server.rs
+++ src/http_server.rs
@@ -1,12 +1,20 @@
 use axum::{Router, response::IntoResponse, Json, routing::get};
 
+use crate::db::DB;
+
 pub fn build_router() -> Router {
     let r = Router::new()
-        .route("/health", get(health));
-
+        .route("/health", get(health))
+        .route("/query-rooms", get(query_rooms));
     r
 }
 
 async fn health() -> impl IntoResponse {
     Json("Health")
+}
+
+async fn query_rooms() -> impl IntoResponse {
+    let db = DB.lock().await;
+    let v = serde_json::json!(*db);
+    Json(v)
 }
